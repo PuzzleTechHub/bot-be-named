a78a1fb60693dc27eb2fe93c9c6f03dd417d852c
kevslinger <siouxman@me.com>
2021-03-26 21:37:55 -0400

    Adding archivecategory NEEDS CLEANING

--- modules/archive_channel/cog.py
+++ modules/archive_channel/cog.py
@@ -38,7 +38,10 @@ class ArchiveChannelCog(commands.Cog, name="Archive Channel"):
         # Check if the user supplied a channel
         if len(args) > 0:
             # Allows them to do e.g. ~archivechannel #MH-general
-            channel_id = int(args[0].replace('>', '').replace('<#', ''))
+            if isinstance(args[0], str):
+                channel_id = int(args[0].replace('>', '').replace('<#', ''))
+            else:
+                channel_id = args[0]
             channel = self.bot.get_channel(channel_id)
             # Write the chat log. Replace attachments with their filename (for easy reference)
             with open(os.path.join(ARCHIVE, TEXT_LOG_PATH), 'w') as f:
@@ -86,15 +89,19 @@ class ArchiveChannelCog(commands.Cog, name="Archive Channel"):
             embed.add_field(name=f"{constants.FAILED}!", value=f"You must specify a channel!")
             await ctx.send(embed=embed)
 
-        #messages = await channel.history(limit=None, oldest_first=True).flatten()
-        #print(len(messages))
-        #time.sleep(2)
-        #for msg in messages:
-        #    print(f"{msg.created_at.strftime('%m/%d/%Y, %H:%M:%S')} - {msg.author.display_name}: {msg.clean_content}")
-        #with open('archived_channel', 'w') as f:
-        #async for msg in channel.history(limit=None, oldest_first=True):
-        #    print(f"{msg.created_at} - {msg.author.display_name}: {msg.clean_content}")
-        #    f.write(f"{msg.created_at} - {msg.author.display_name}: {msg.clean_content}")
+    @commands.command(name="archivecategory")
+    async def archivecategory(self, ctx, *args):
+        """Command to download the history of every text channel in the category"""
+        print("Received archivecategory")
+        embed = utils.create_embed()
+        # Check if the user supplied a channel
+        if len(args) > 0:
+            channel_id = int(args[0].replace('>', '').replace('<#', ''))
+            channel = self.bot.get_channel(channel_id)
+            print(channel.name)
+            print(channel.text_channels)
+            for text_channel in channel.text_channels:
+                await self.archivechannel(ctx, text_channel.id)
 
 
 def setup(bot):
