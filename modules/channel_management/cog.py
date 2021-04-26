4d8fa7a8bc109893180c6fe090b197a145fc4b4e
Soni-Mantis <the.original.soni@gmail.com>
2021-04-27 04:35:25 +0530

    Merge all channel management fucntions

---
+++ modules/channel_management/cog.py
@@ -0,0 +1,104 @@
+import constants
+import discord
+from discord.ext import commands
+from utils import discord_utils
+
+# Big thanks to denvercoder1 and his professor-vector-discord-bot repo
+# https://github.com/DenverCoder1/professor-vector-discord-bot
+class ChannelManagementCog(commands.Cog, name="Channel Management"):
+    """Checks for `movechannel` command
+    Moves current channel to given category"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="movechannel")
+    @commands.has_any_role(
+        constants.VERIFIED_PUZZLER,
+        constants.TA_VERIFIED_PUZZLER_ROLE_ID
+    )
+    async def movechannel(self, ctx, *args):
+        """Command to move channel to category with given name"""
+        print("Received movechannel")
+        embed = discord_utils.create_embed()
+        # check for category name arguments
+        if len(args) > 0:
+            # join arguments to form channel name
+            category_name = " ".join(args)
+            # get current channel
+            channel = ctx.channel
+            # get new category
+            new_category = discord.utils.get(ctx.guild.channels, name=category_name)
+            if new_category is not None:
+                embed.add_field(name=f"{constants.SUCCESS}!", value=f"Moving {channel.mention} to {new_category}!")
+                # reply to user
+                await ctx.send(embed=embed)
+                # move channel
+                await ctx.channel.edit(category=new_category)
+            else:
+                embed.add_field(name=f"{constants.FAILED}!", value=f"Could not find category `{category_name}`")
+                # reply to user
+                await ctx.send(embed=embed)
+        # no argument passed
+        else:
+            embed.add_field(name=f"{constants.FAILED}!", value=f"You must specify a category!")
+            # reply to user
+            await ctx.send(embed=embed)
+
+
+    @commands.command(name="createchannel")
+    @commands.has_any_role(
+        constants.VERIFIED_PUZZLER,
+        constants.TA_VERIFIED_PUZZLER_ROLE_ID
+    )
+    async def createchannel(self, ctx, name: str = ""):
+        """Command to create channel in same category with given name"""
+        # log command in console
+        print("Received createchannel")
+        embed = discord_utils.create_embed()
+        # no argument passed
+        if len(name) <= 0:
+            embed.add_field(name="Failed!", value=f"You must specify a channel name!")
+            # reply to user
+            await ctx.send(embed=embed)
+            return
+        # get guild and category
+        guild = ctx.message.guild
+        category = ctx.channel.category
+        # create channel
+        channel = await guild.create_text_channel(name, category=category)
+        embed.add_field(name="Success!", value=f"Created channel {channel.mention} in {category}!")
+        # reply to user
+        await ctx.send(embed=embed)
+
+    @commands.command(name="clonechannel")
+    @commands.has_any_role(
+        constants.VERIFIED_PUZZLER,
+        constants.TA_VERIFIED_PUZZLER_ROLE_ID
+    )
+    async def clonechannel(self, ctx, original: str = "", new: str = ""):
+        """Command to create channel in same category with given name"""
+        # log command in console
+        print("Received clonechannel")
+        embed = discord_utils.create_embed()
+        # no argument passed
+        if len(original) <= 1:
+            embed.add_field(name="Failed!", value=f"You must specify a channel to clone and the new channel name!")
+            # reply to user
+            await ctx.send(embed=embed)
+            return
+        # get guild and category
+        guild = ctx.message.guild
+        category = ctx.channel.category
+        old_channel = discord_utils.find_channel(self.bot, guild.channels, original)
+        # create channel
+        new_channel = await guild.create_text_channel(new, category=category, overwrites=old_channel.overwrites)
+
+        
+        embed.add_field(name="Success!", value=f"Created channel {new_channel.mention} in {category}!")
+        # reply to user
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(ChannelManagementCog(bot))
--- modules/clone_channel/README.md
+++
@@ -1,7 +0,0 @@
-# Clone Channel
-
-Create a new channel with the same permissions as the channel mentioned
-
-- `~clonechannel #current-channel new-channel-name` will create the 
-channel with the same permissions as `#current-channel` with name
-  `new-channel-name` in the category where the command was called.
