dea05bf0b5a6dbe9bc174435ffe0462ad305fede
kevslinger <siouxman@me.com>
2021-05-30 09:38:17 -0400

    Output combined weird tunes on user incorrect guess

--- modules/music_race/cog.py
+++ modules/music_race/cog.py
@@ -3,9 +3,10 @@ import discord
 import re
 from discord.ext import commands
 from utils import discord_utils
-import modules.perfect_pitch
+from modules.perfect_pitch import perfect_pitch_constants
 import os
 import numpy as np
+import string
 
 answers = ["FROZEN", "STARWARS", "ROCKY", "XMEN", "TITANIC", "HARRYPOTTER", "JAMESBOND", "LIONKING", "AVENGERS"]
 answers_indexes = [3, 2, 3, 2, 7, 1, 7, 2, 7]
@@ -29,9 +30,9 @@ def get_partition_mapping():
     for answer in answers:
         for idx, letter in enumerate(answer):
             if letter in map:
-                map[letter].append(f"{answer}_part_{idx}.mp3")
+                map[letter].append(f"{answer}_part_{idx}")
             else:
-                map[letter] = [f"{answer}_part_{idx}.mp3"]
+                map[letter] = [f"{answer}_part_{idx}"]
     return map
 
 
@@ -42,17 +43,6 @@ class MusicRace(commands.Cog, name="Music Race"):
         self.bot = bot
         self.letters = get_letters()
         self.partition_map = get_partition_mapping()
-
-
-    async def correct_answer(self, ctx, word, idx):
-        """Answer found is correct"""
-        print("Recieved correct_answer")
-        embed = discord_utils.create_embed()
-
-        """TODO : Send correct playtune... with appropriate number of rests (for indexing)"""
-        """TODO : Send correct song with name idx/11.mp3"""
-
-
 
     @commands.command(name="puzzleplaceholder")
     @commands.has_any_role(
@@ -101,20 +91,47 @@ class MusicRace(commands.Cog, name="Music Race"):
                     finalanswer.append((answer.lower(), i*3))
                     delay = i * 3
                     break
+            if delay > 0: # If we found a match
+                break
+        print(finalanswer)
         # At this point, we've matched all we can to the song.
         # i is now the index of the longest substring that matches
         # So we need to start from the ith character
         for j in range(i, len(word)):
-            c = word[i]
-            if c not in self.partition_map:
+            char = word[j]
+            if char not in self.partition_map:
                 delay += 3
                 continue
             else:
-                finalanswer.append(np.random.choice(self.partition_map[c]), delay)
+                finalanswer.append((np.random.choice(self.partition_map[char]).lower(), delay))
                 delay += 3
-        
+
         print(self.letters)
-        await ctx.send(f"{self.letters}")
+        debug_output_msg = f"0-{finalanswer[0][1]}: {finalanswer[0][0]}\n"
+        for ans in finalanswer[1:]:
+            debug_output_msg += f"{ans[1]}-{ans[1]+3}: {ans[0]}\n"
+
+        await ctx.send(debug_output_msg)
+        print(finalanswer)
+
+        inputs = ''.join([f"-i {os.path.join(os.getcwd(), 'modules', 'perfect_pitch', 'music', 'puzzle_songs', finalanswer[idx][0] + '.mp3')} " for idx in range(len(finalanswer))])
+        filter_complex = f"[0]atrim=0:{finalanswer[0][1]},adelay=0|0[a];"
+        filter_complex += "".join([f"[{idx+1}]adelay={finalanswer[idx+1][1]*1000}|{finalanswer[idx+1][1]*1000}[{letter}];"
+                                  for idx, letter in zip(range(len(finalanswer[1:])), string.ascii_lowercase[1:])])
+        mix = ''.join([f"[{letter}]" for _, letter in zip(finalanswer, list(string.ascii_lowercase))])
+        print(inputs)
+        print(filter_complex)
+        print(mix)
+        print(len(finalanswer))
+        if not os.path.exists(os.path.join(os.getcwd(), 'modules', 'perfect_pitch', 'music', 'puzzle_outputs', ctx.channel.name)):
+            os.mkdir(os.path.join(os.getcwd(), 'modules', 'perfect_pitch', 'music', 'puzzle_outputs', ctx.channel.name))
+        os.system(
+            f"ffmpeg -y  {inputs} " +
+            f"-filter_complex '{filter_complex}{mix}amix=inputs={len(finalanswer)}:dropout_transition=1000,volume={perfect_pitch_constants.VOLUME/2}' "
+            f"{os.path.join(os.getcwd(), 'modules', 'perfect_pitch', 'music', 'puzzle_outputs', ctx.channel.name, 'jam.mp3')}"
+        )
+        await ctx.send(file=discord.File(os.path.join(os.getcwd(), 'modules', 'perfect_pitch', 'music', 'puzzle_outputs', ctx.channel.name, 'jam.mp3')))
+
 
 def setup(bot):
     bot.add_cog(MusicRace(bot))
