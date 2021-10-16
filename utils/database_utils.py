f00c64b111f403fb3374c5acb5d38800ffa9d5d8
kevslinger <esslinger.k@northeastern.edu>
2021-10-16 13:34:35 -0400

    For some reason heroku griefs and uses postgres:// instead of postgresql://

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -3,16 +3,9 @@ from sqlalchemy import Column, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.sqltypes import BIGINT, Integer, String
 
-# text, Table, Column, Integer, String, insert, MetaData
-# from sqlalchemy.orm import Session
-
-# with Session(engine) as session:
-#     result = session.execute(text("SELECT * FROM verifieds where server_id='820327073213186079'"))
-#     for row in result:
-#         print(f"Verified role ids: {','.join(row['verified_role_ids'])}")
 
 def create_database_engine():
-    return create_engine(os.getenv("DATABASE_URL"), echo=True, future=True)
+    return create_engine(os.getenv("POSTGRES_DB_URL"), echo=True, future=True)
 
 
 Base = declarative_base()
