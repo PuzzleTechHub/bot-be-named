8a2c2f010b4058029d40c1d70fe582d5d6e11540
TheOriginalSoni <the.original.soni@gmail.com>
2022-04-21 14:02:31 +0530

    Add same to metacrab etc as well

--- modules/sheets/sheet_utils.py
+++ modules/sheets/sheet_utils.py
@@ -171,6 +171,8 @@ async def metacrabgeneric(gspread_client, ctx, chan_name: str, *args):
         inline=False,
     )
     await ctx.send(embed=embed)
+
+    addsheettethergeneric(gspread_client,curr_sheet_link,ctx.message.guild,new_chan)
     return curr_sheet_link, newsheet, new_chan
 
 
