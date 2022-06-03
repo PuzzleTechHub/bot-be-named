740a3c908cb5e55a215795ec67b027cc484fed23
Jonah Lawrence <jonah@freshidea.com>
2022-06-02 20:41:25 -0600

    feat: confessional channels

---
+++ modules/confessional_request/cog.py
@@ -0,0 +1,53 @@
+import nextcord
+from nextcord.ext import commands
+
+from .create_channel import CreateChannelView
+from .select_category import SelectCategoryView
+
+
+class ConfessionalRequest(commands.Cog, name="Confessional Request"):
+    """Channel request button system"""
+
+    def __init__(self, bot: commands.Bot):
+        self._bot = bot
+        self._persistent_view_added = False
+        self.BUTTON_TEXT = "Create Confessional Channel"
+        self.EMBED_TITLE_SUFFIX = " Channel Request"
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self._persistent_view_added:
+            self._persistent_view_added = True
+            button_view = CreateChannelView(self.BUTTON_TEXT, self.EMBED_TITLE_SUFFIX)
+            self._bot.add_view(button_view)
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def ticketbtn(self, ctx: commands.Context):
+        """Creates a button for creating a temporary channel"""
+        if ctx.guild is None:
+            return await ctx.send("This command can only be used in a server")
+        # prompt for a category
+        category_view = SelectCategoryView(categories=ctx.guild.categories, ctx=ctx)
+        category_msg = await ctx.reply(f"Please select a category", view=category_view)
+        await category_view.wait()
+        category = category_view.dropdown.selected_category
+        await category_msg.delete()
+        if category is None:
+            return await ctx.send("No category selected", delete_after=3)
+        # create the button view
+        button_view = CreateChannelView(
+            label=self.BUTTON_TEXT,
+            title_suffix=self.EMBED_TITLE_SUFFIX,
+            ctx=ctx,
+        )
+        embed = nextcord.Embed(
+            title=f"{category.name}{self.EMBED_TITLE_SUFFIX}",
+            description=f'If you want to write confessionals in {category.name}, click "{self.BUTTON_TEXT}" below. This will make a private confessional channel for you where spectators and dead players can read your thoughts!',
+            color=0x009999,
+        )
+        await ctx.send(embed=embed, view=button_view)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(ConfessionalRequest(bot))
---
+++ modules/confessional_request/create_channel.py
@@ -0,0 +1,87 @@
+from typing import Optional
+
+import nextcord
+from nextcord.ext import commands
+from numpy import isin
+
+
+class CreateChannelButton(nextcord.ui.Button["CreateChannelView"]):
+    def __init__(self, label: str, title_suffix: str):
+        self._title_suffix = title_suffix
+        super().__init__(label=label, custom_id=f"ticket:create")
+
+    async def callback(self, interaction: nextcord.Interaction):
+        """Callback for the button. Creates a new channel in the category for the user"""
+        assert (
+            interaction.guild is not None
+            and interaction.message is not None
+            and isinstance(interaction.user, nextcord.Member)
+            and len(interaction.message.embeds) > 0
+            and isinstance(interaction.message.embeds[0].title, str)
+        )
+        # defer so we can take some time to create the channel
+        await interaction.response.defer()
+        # get the category
+        embed_title = interaction.message.embeds[0].title
+        category_name = embed_title.removesuffix(self._title_suffix)
+        category = nextcord.utils.find(
+            lambda c: c.name == category_name, interaction.guild.categories
+        )
+        if category is None:
+            return await interaction.send(
+                f"Could not find the category {category_name}. Please contact an administrator.",
+                ephemeral=True,
+            )
+        # create the channel
+        channel = await category.create_text_channel(
+            f"{interaction.user.name}",
+            reason=f"{interaction.user.name} requested a channel",
+            topic=f"{interaction.user.mention}'s channel",
+        )
+        # give the user manage messages permission on the channel
+        await channel.set_permissions(interaction.user, manage_messages=True)
+        # send a message to the channel
+        embed = nextcord.Embed(
+            description=f"""Welcome to your private confessional channel where spectators and dead players can read your thoughts! You can say anything here regarding the game you are playing in!
+
+Please note that this channel can be seen by the players of the other game, so you may not talk about any aspects of the other game in here, and you may lose spectating permissions or face removal from the game if you break the rules.
+
+If you have a question, please ping <@981849279897428048>!""",
+            color=0x009999,
+        )
+        embed.set_author(
+            name=f"{interaction.user.name}",
+            icon_url=interaction.user.display_avatar.url,
+        )
+        await channel.send(
+            content=f"{interaction.user.mention} has created a confessional channel!",
+            embed=embed,
+        )
+        # respond to the button interaction
+        embed = nextcord.Embed(
+            title="Your channel has been created",
+            description=f"You can now write confessionals in {channel.mention}",
+            color=0x009999,
+        )
+        await interaction.send(embed=embed, ephemeral=True)
+
+
+class CreateChannelView(nextcord.ui.View):
+    def __init__(
+        self,
+        label: str,
+        title_suffix: str,
+        ctx: Optional[commands.Context] = None,
+    ):
+        """View for the create channel button
+
+        Arguments:
+            label (str): The label of the button
+            title_suffix (str): The suffix to append to the title.
+            ctx (Optional[commands.Context]): The context of the command.
+                This does not need to be supplied when adding this as a persistent view.
+        """
+        super().__init__(timeout=None)
+        self._ctx = ctx
+        self._button = CreateChannelButton(label=label, title_suffix=title_suffix)
+        self.add_item(self._button)
