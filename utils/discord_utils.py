7f86c5949777f6faa321948bf9c36430fc660ecf
TheOriginalSoni <the.original.soni@gmail.com>
2025-11-29 11:06:34 +0530

    Add split handling for empty embeds

--- utils/discord_utils.py
+++ utils/discord_utils.py
@@ -252,11 +252,13 @@ def split_embed(embed: nextcord.Embed) -> List[nextcord.Embed]:
             characters_remaining -= cutoff_point
     # If the title + description are small, we can just copy them over
     else:
-        embed_list.append(
-            nextcord.Embed(
-                title=embed_title, description=embed.description, color=embed.color
-            )
-        )
+        # embed_list.append(
+        #    nextcord.Embed(
+        #        title=embed_title, description=embed.description, color=embed.color
+        #    )
+        # )
+        return [embed]
+
     character_count = len(embed_list[-1].title) + len(embed_list[-1].description)
 
     # Iterate over all the proposed fields in the embed
