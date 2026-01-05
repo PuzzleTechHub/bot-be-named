9345f7b02a00bb86788bf488e7a59f34b5588649
Soni <the.original.soni@gmail.com>
2026-01-05 12:13:30 +0530

    Delete modules/hydra/flag_converter.py
    No need for file anymore if we're not committing to Flag_converter immediately

--- modules/hydra/flag_converter.py
+++
@@ -1,7 +0,0 @@
-# from nextcord.ext import commands
-# from typing import Optional
-
-# class chanhydra(commands.FlagConverter, delimiter=": ", prefix=""):
-#     puzzle_name: str
-#     template_name: str
-#     puzzle_url: Optional[str]
